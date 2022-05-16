1b03f7f86fe06d31eea6df83f8af3bc9e90d4a69
Hirisci <www.alan92@icloud.com>
2022-05-16 12:22:40 +0200

    fatto

---
+++ snack-1/main.js
@@ -0,0 +1,23 @@
+// Crea un array di 10 oggetti che rappresentano una zucchina, indicando per ognuna varietà, peso e lunghezza.
+// Calcola quanto pesano tutte le zucchine.
+
+class Zucchina {
+    constructor(height, width,type) {
+      this.height = height;
+      this.width = width;
+      this.type = type;
+    }
+  }
+  
+const zuccchina = new Zucchina("verde",520,20)
+const zuccchina1 = new Zucchina("rigata",520,20)
+const zuccchina2 = new Zucchina("tonda",520,20)
+const zuccchina3 = new Zucchina("trombetta",520,20)
+const zuccchina4 = new Zucchina("sicilina",520,20)
+const zuccchina5 = new Zucchina("nera",520,20)
+const zuccchina6 = new Zucchina("milano",520,20)
+const zuccchina7 = new Zucchina("faenza",520,20)
+const zuccchina8 = new Zucchina("roamana",520,20)
+const zuccchina9 = new Zucchina("toscana",520,20)
+
+console.log(zuccchina0)
