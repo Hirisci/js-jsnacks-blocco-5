5b8722fd95ee9e834df3dc7f59baf897be62b018
Hirisci <www.alan92@icloud.com>
2022-05-16 12:53:45 +0200

    bug fix

--- snack-2/main.js
+++ snack-2/main.js
@@ -10,15 +10,15 @@ class Zucchina {
   }
   
 const arr = [new Zucchina("verde",520,16),
-                    new Zucchina("rigata",420,3),
-                    new Zucchina("tonda",620,3),
-                    new Zucchina("trombetta",425,4),
-                    new Zucchina("sicilina",320,20),
-                    new Zucchina("nera",220,16),
-                    new Zucchina("milano",320,9),
-                    new Zucchina("faenza",420,12),
-                    new Zucchina("roamana",620,10),
-                    new Zucchina("toscana",720,5)]
+             new Zucchina("rigata",420,3),
+             new Zucchina("tonda",620,3),
+             new Zucchina("trombetta",425,4),
+             new Zucchina("sicilina",320,20),
+             new Zucchina("nera",220,16),
+             new Zucchina("milano",320,9),
+             new Zucchina("faenza",420,12),
+             new Zucchina("roamana",620,10),
+             new Zucchina("toscana",720,5)]
 
 let arrMaggiore = [];
 let arrMinore = []
@@ -34,7 +34,7 @@ let totale = 0
 for (let i = 0; i < arrMaggiore.length; i++) {
     totale += arrMaggiore[i].height;    
 }
-
+totale=0;
 let msg = `Il peso delle zucchine che misurano più di 15cm è di ${totale} grammi`
 for (let i = 0; i < arrMinore.length; i++) {
   totale += arrMinore[i].height;    
